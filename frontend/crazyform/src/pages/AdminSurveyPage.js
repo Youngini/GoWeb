456de4589fbaca6dd56ac66cfaca1ccac45640a6
diwoni <jgw117@naver.com>
2024-06-09 22:04:16 +0900

    feat : Logo 컴포넌트 추가 및 css 요소 변경

--- frontend/crazyform/src/pages/AdminSurveyPage.js
+++ frontend/crazyform/src/pages/AdminSurveyPage.js
@@ -1,9 +1,10 @@
 import AdminTopNavbar from "../components/AdminTopNavBar";
 import CategoryList from '../components/CategoryList'
-
+import Logo from "../components/logo";
 const AdminSurveyPage = () => {
     return(
         <div>
+            <Logo />
             <AdminTopNavbar />
             <CategoryList></CategoryList>
         </div>
