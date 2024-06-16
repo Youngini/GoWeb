21e1858815de6a917c31be359031a42c607de1fa
young-in <hardworkingini@gmail.com>
2024-06-16 20:40:36 +0900

    [feat] Controller 추가

---
+++ src/main/java/Goweb/FormMaker/controller/survey/QuestionController.java
@@ -0,0 +1,11 @@
+package Goweb.FormMaker.controller.survey;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("questions")
+public class QuestionController {
+}
---
+++ src/main/java/Goweb/FormMaker/controller/user/UserController.java
@@ -0,0 +1,11 @@
+package Goweb.FormMaker.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("users")
+public class UserController {
+}
