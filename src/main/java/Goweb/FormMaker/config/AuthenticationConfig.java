bd4ab1c7d4f103f6e1bbe14dc14a1476af7e7119
jinno Yun <jinno321@naver.com>
2024-06-23 05:45:46 +0900

    feat: Authentication Config

---
+++ src/main/java/Goweb/FormMaker/config/AuthenticationConfig.java
@@ -0,0 +1,39 @@
+package Goweb.FormMaker.config;
+
+import Goweb.FormMaker.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AuthenticationConfig {
+
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/admins/**").hasRole("ADMIN")
+                .requestMatchers("/auths/**", "/image/**", "/v3/**", "/swagger-ui/**", "/v3/api-docs").permitAll()
+                .requestMatchers("/**").authenticated()
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
