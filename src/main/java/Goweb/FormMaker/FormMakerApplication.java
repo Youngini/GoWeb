08417a01c2dc6d44704f15b612249528675928a0
jinno Yun <jinno321@naver.com>
2024-06-24 00:25:15 +0900

    sync

--- src/main/java/Goweb/FormMaker/FormMakerApplication.java
+++ src/main/java/Goweb/FormMaker/FormMakerApplication.java
@@ -2,9 +2,11 @@ package Goweb.FormMaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
+
 public class FormMakerApplication {
 
 	public static void main(String[] args) {
