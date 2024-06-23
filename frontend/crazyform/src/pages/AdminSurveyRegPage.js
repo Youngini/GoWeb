68dcc0cbaa2d51a226588d58c17c02b6412c3f1f
diwoni <jgw117@naver.com>
2024-06-23 21:21:27 +0900

    feat : 홈페이지 폰트 적용 및 디자인 전체 다듬기 완료

--- frontend/crazyform/src/pages/AdminSurveyRegPage.js
+++ frontend/crazyform/src/pages/AdminSurveyRegPage.js
@@ -1,32 +1,23 @@
-import { useState } from "react"
-import AdminTopNavbar from "../components/AdminTopNavBar"
-import '../components/style/AdminSurveyRegPage.css'
-import SurveyForm from "../components/SurveyForm"
+import { useState } from "react";
+import AdminTopNavbar from "../components/AdminTopNavBar";
+import "../components/style/AdminSurveyRegPage.css";
+import SurveyForm from "../components/SurveyForm";
 
 const AdminSurveyReg = () => {
-
-    return(
-        <div>
-            <AdminTopNavbar />
-            <div className="surveydetailpageContent">
-                <div className="buttonContent">
-                    <div className={'buttonWrap'}>
-                        김영인
-                    </div>
-                    <div className={'buttonWrap'}>
-                        김현수 
-                    </div>
-                    <div className={'buttonWrap'}>
-                        윤진노
-                    </div>
-                    <div className={'buttonWrap'}>
-                        정지원
-                    </div>
-                </div>
-                    <SurveyForm />
-            </div>
+  return (
+    <div>
+      <AdminTopNavbar />
+      <div className="surveydetailpageContent">
+        <div className="buttonContent">
+          <div className={"buttonWrap"}>김영인</div>
+          <div className={"buttonWrap"}>김현수</div>
+          <div className={"buttonWrap"}>윤진노</div>
+          <div className={"buttonWrap"}>정지원</div>
         </div>
-    )
-}
+        <SurveyForm />
+      </div>
+    </div>
+  );
+};
 
-export default AdminSurveyReg
+export default AdminSurveyReg;
--- frontend/crazyform/src/pages/HomePage.js
+++ frontend/crazyform/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Modal from 'react-modal';
+import '../components/style/HomePage.css';
 
 const HomePage = () => {
     const navigate = useNavigate();
